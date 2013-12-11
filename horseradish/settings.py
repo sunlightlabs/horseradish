e44d5d380e154cbc4cbd2e913d9a461ca90e87a3
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-12-11 15:06:17 -0500

    Add basic logging

--- horseradish/settings.py
+++ horseradish/settings.py
@@ -149,3 +149,30 @@ PHOTO_SOURCES = [(s, s) for s in sorted((
 
 GOOGLEAUTH_IS_STAFF = False
 GOOGLEAUTH_DOMAIN = os.environ.get('GOOGLEAUTH_DOMAIN')
+
+
+# logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
